7f55d5878a1141fba399357d807de4a7cdc13ee3
redvey <roopam12cse@gmail.com>
2023-11-05 20:22:14 +0530

    #1 Adding Basic files like Palette

---
+++ lib/consts/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static final Color background = Color.fromRGBO(30, 29, 42, 1.0);
+  static final Color navBar = Color.fromRGBO(37, 39, 54, 1.0);
+  static final Color cards = Color.fromRGBO(37, 39, 54, 1.0);
+  static final Color hydrationLevelCard = Color.fromRGBO(1, 119, 251, 1.0);
+  static final Color patientCard = Color.fromRGBO(46, 48, 62, 1.0);
+}
